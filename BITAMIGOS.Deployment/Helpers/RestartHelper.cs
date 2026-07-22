87866901a856d7fb080c2523b04b10362107027d
BITAMIGOS | Christoph <christoph@bitamigos.at>
2026-07-22 11:28:14 +0200

    fix: use Wpeutil rebbot in WinPE

--- BITAMIGOS.Deployment/Helpers/RestartHelper.cs
+++ BITAMIGOS.Deployment/Helpers/RestartHelper.cs
@@ -1,26 +1,16 @@
 ﻿using BITAMIGOS.Deployment.Helpers;
 
-namespace BITAMIGOS.Deployment.Helpers;
+Console.WriteLine();
+Console.Write("PC jetzt neu starten? (J/N): ");
 
-public static class RestartHelper
+string? input = Console.ReadLine();
+
+if (input?.Equals("J",
+    StringComparison.OrdinalIgnoreCase) == true)
 {
-    public static void AskForRestart()
-    {
-        Console.WriteLine();
-        Console.Write("PC jetzt neu starten? (J/N): ");
+    Logger.Info("Starte Computer neu...");
 
-        string? input = Console.ReadLine();
-
-        if (input == null)
-            return;
-
-        if (input.Equals("J", StringComparison.OrdinalIgnoreCase))
-        {
-            Logger.Info("Starte Computer neu...");
-
-            ProcessRunner.Run(
-                "shutdown.exe",
-                "/r /t 0");
-        }
-    }
+    ProcessRunner.Run(
+        "wpeutil.exe",
+        "reboot");
 }
